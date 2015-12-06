9c1339296b05018e4d091c93005a3715b780f687
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2015-12-06 18:41:48 +0530

    Basic configuration of app

---
+++ compile/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+#from app_name.models import model_name
+# Register your models here.
+#admin.site.register(model_name)
---
+++ compile/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns,url
+from compile import views
+
+urlpatterns=patterns(
+'',
+#url(r'^leaderboard/',views.fetch_leaderboard),
+)
