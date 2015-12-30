c3b4fd1d45651bf35489d3513800edf2e83c39b2
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2015-12-30 12:02:44 +0530

    Changed db to SQLite3

--- coding_companion/settings.py
+++ coding_companion/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import connections
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -74,10 +73,8 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': connections.db,
-    'USER':connections.user,
-    'PASSWORD':connections.password,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
